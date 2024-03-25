709833458b27d5deee761e5d067b325dc8d5ad24
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-25 10:02:24 -0300

    feat: salesforce icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Login from './components/Login/login'
+import Header from './components/Header/index'
 
 function App() {
 
   return (
     <>
+        <Header/>
         <Login/>
     </>
   )
