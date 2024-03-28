ecd4d05561258d11bac53ac05e667b14710131a8
luiz alecsander viana <luiz.alecsander@gmail.com>
2024-03-28 17:59:56 -0300

    feat: Finished integrantes Page

--- src/pages/Integrantes/index.tsx
+++ src/pages/Integrantes/index.tsx
@@ -2,6 +2,9 @@ import './style.css'
 import Header from '../../components/Header'
 import CardIntegrantes from '../../components/CardIntegrantes'
 import IntegranteLuizFoto from '../../assets/luizFoto.svg'
+import IntegranteNatanFoto from '../../assets/natanFoto.svg'  
+import IntegranteGuilhermeFoto from '../../assets/guilhermeFoto.svg'
+
 
 const Integrantes = () => {
     return (
@@ -11,7 +14,8 @@ const Integrantes = () => {
             <h1 className='title'>Integrantes</h1>
             </div>
             <CardIntegrantes nome = "Luiz Alecsander" fotoNome = {IntegranteLuizFoto} numeroRM='553034'/>
-            
+            <CardIntegrantes nome = "Natan Junior" fotoNome = {IntegranteNatanFoto} numeroRM='552626'/>
+            <CardIntegrantes nome = "Guilherme Augusto" fotoNome = {IntegranteGuilhermeFoto} numeroRM='554176'/>
 
         </main>
         
